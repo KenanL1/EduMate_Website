5a99a28502a713395029eaf1c5ca05e0b0b86b51
illusionXD <36828222+illusionXD@users.noreply.github.com>
2020-10-15 12:46:38 -0400

    Initial commit

---
+++ Web/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def index(request):
+    return render(request, "Web/index.html")
